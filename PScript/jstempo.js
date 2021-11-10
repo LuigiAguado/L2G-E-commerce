9ade5e25136d3bb2c00fa1ac90f63ed8e2c3b6b0
Gabriel Arthur Tonholo de Campos <gabrielarthurtonholodecampos@gmail.com>
2021-11-10 19:00:48 -0300

    Mudança do nome da Pasta principal

--- PScript/jstempo.js
+++
@@ -1,9 +0,0 @@
-{
-            var d = new Date();
-            var date = d.getDate();
-            var year = d.getFullYear();
-            var month = d.getMonth();
-            var monthArr = ["Janeiro", "Fevereiro","Março", "Abril", "maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro","Dezembro"];
-            month = monthArr[month];
-            document.getElementById("date").innerHTML="Jundiaí, "+date+" de "+month+" de "+year+".";
-        }
